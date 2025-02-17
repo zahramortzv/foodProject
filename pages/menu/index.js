b2072e4a58422910fe6aee83764a6a93480baa8f
zahra mortazavi <zahramortazavi23@gmail.com>
2025-02-17 11:05:00 +0330

    build final

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -11,6 +11,6 @@ export async function getStaticProps() {
 
     return {
         props: { data },
-        revalidate: +process.env.REVALIDATE, //seconds seconds
+        revalidate: +process.env.REVALIDATE, //seconds 
     };
 }
