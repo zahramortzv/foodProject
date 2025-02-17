59380be8c43a4f8405c90b2d4d13759dbf5b7d71
zahra mortazavi <zahramortazavi23@gmail.com>
2025-02-17 10:46:31 +0330

    build again

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,21 +1,16 @@
-import MenuPage from '@/components/templates/MenuPage'
+import MenuPage from "@/components/templates/MenuPage";
 
 function Menu({ data }) {
-    return (
-        <MenuPage data={data} />
-    )
+    return <MenuPage data={data} />;
 }
-
-export default Menu
+export default Menu;
 
 export async function getStaticProps() {
     const res = await fetch(`${process.env.BASE_URL}/data`);
-    const data = await res.json()
+    const data = await res.json();
 
     return {
         props: { data },
-        revalidate: +process.env.REVALIDATE
-
-    }
-
+        revalidate: +process.env.REVALIDATE, //seconds
+    };
 }
